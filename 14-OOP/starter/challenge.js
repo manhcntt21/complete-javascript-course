497c3d37cc270138b9af800c07318cd637360f17
Do Truong Manh <manhcntt2.1@gmail.com>
2023-08-03 17:52:41 +0700

    14: OOP : codeing challenge #2

--- 14-OOP/starter/challenge.js
+++ 14-OOP/starter/challenge.js
@@ -22,3 +22,38 @@ car1.accelerate();
 car1.brake();
 car2.accelerate();
 car2.brake();
+
+// challenge #2
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+  }
+
+  brake() {
+    this.speed -= 5;
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed / 1.6;
+  }
+}
+
+const carCl = new CarCl('Ford', 120);
+
+console.log(carCl);
+carCl.accelerate();
+console.log(carCl);
+carCl.brake();
+console.log(carCl);
+console.log(carCl.speedUS);
+carCl.speedUS = 160;
+console.log(carCl);
