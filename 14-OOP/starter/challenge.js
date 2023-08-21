0aba8dbfece4037a6a743bef53a5c8319b79dd54
Do Truong Manh <manhcntt2.1@gmail.com>
2023-08-21 14:14:36 +0700

    14: ÔOP: Coding challenge #3

--- 14-OOP/starter/challenge.js
+++ 14-OOP/starter/challenge.js
@@ -57,3 +57,30 @@ console.log(carCl);
 console.log(carCl.speedUS);
 carCl.speedUS = 160;
 console.log(carCl);
+
+// challenge #3
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+// link prototype
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge -= 1;
+  console.log(
+    `${this.make} going at ${this.speed} km/h with a charge ${this.charge}`
+  );
+};
+
+const evCar = new EV('Tesla', 120, 23);
+evCar.accelerate();
+evCar.brake();
+evCar.chargeBattery(90);
+
+evCar.accelerate();
