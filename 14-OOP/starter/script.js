63f6437c862c4de0d7c500e3e57461446d5bd8e3
Do Truong Manh <manhcntt2.1@gmail.com>
2023-08-21 15:46:28 +0700

    14: OOP: fake encapsulation

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -258,3 +258,51 @@ const jay2 = Object.create(StudentProto);
 jay2.init('Jay', 2010, 'Computer Science');
 jay2.introduce();
 jay2.calcAge();
+
+// 222 Another Class Example
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    // fake encapsulation
+    this._pin = pin;
+    // fake encapsulation
+    this._movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening account, ${this.owner}`);
+  }
+  // puclic interface - a API
+  getMovements() {
+    return this._movements;
+  }
+  deposit(val) {
+    this._movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+  _approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 11111);
+// console.log(acc1);
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+
+console.log(acc1.getMovements());
+console.log(acc1);
+console.log(acc1.pin);
