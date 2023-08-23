52855d28912309558d232b244338a061527fa157
Do Truong Manh <manhcntt2.1@gmail.com>
2023-08-23 20:03:35 +0700

    16: consuming Promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -17,27 +17,27 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 
 // // old school way of AJAX call - XMLHttpRequest
-// const renderCountry = function (data, className = '') {
-//   const html = `
-//             <article class="country ${className}">
-//               <img class="country__img" src="${data.flags.svg}" />
-//               <div class="country__data">
-//                 <h3 class="country__name">${data.name.common}</h3>
-//                 <h4 class="country__region">${data.region}</h4>
-//                 <p class="country__row"><span>👫</span>${data.population}</p>
-//                 <p class="country__row"><span>🗣️</span>${
-//                   Object.values(data.languages)[0]
-//                 }</p>
-//                 <p class="country__row"><span>💰</span>${
-//                   Object.values(data.currencies)[0].name
-//                 }</p>
-//               </div>
-//             </article>
-//       `;
+const renderCountry = function (data, className = '') {
+  const html = `
+            <article class="country ${className}">
+              <img class="country__img" src="${data.flags.svg}" />
+              <div class="country__data">
+                <h3 class="country__name">${data.name.common}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${data.population}</p>
+                <p class="country__row"><span>🗣️</span>${
+                  Object.values(data.languages)[0]
+                }</p>
+                <p class="country__row"><span>💰</span>${
+                  Object.values(data.currencies)[0].name
+                }</p>
+              </div>
+            </article>
+      `;
 
-//   countriesContainer.insertAdjacentHTML('beforeend', html);
-//   countriesContainer.style.opacity = 1;
-// };
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 // const getCountryAndNeighbour = function (country) {
 //   // AJAX call country 1
 //   const request = new XMLHttpRequest();
@@ -93,9 +93,32 @@ const countriesContainer = document.querySelector('.countries');
 // request.send();
 
 // promises
-const request = fetch('https://restcountries.com/v3.1/name/portugal');
-console.log(request);
+// const request = fetch('https://restcountries.com/v3.1/name/portugal');
+// console.log(request);
 
-setTimeout(() => {
-  console.log(request);
-}, 3000);
+// setTimeout(() => {
+//   console.log(request);
+// }, 3000);
+
+// consume promises
+
+// function declaration
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+// arrow function
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+getCountryData('portugal');
