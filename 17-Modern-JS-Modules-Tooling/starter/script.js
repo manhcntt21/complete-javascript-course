a7a25637df5de99763338e5aefb0e0b210360a86
Do Truong Manh <manhcntt2.1@gmail.com>
2023-09-12 02:49:21 +0700

    17: top-level await works

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -14,11 +14,37 @@
 // console.log(ShoppingCart.tq);
 
 // gan tren cho default export
-import add, { cart } from './shoppingCart.js';
-console.log('Importing module');
+// import add, { cart } from './shoppingCart.js';
+// console.log('Importing module');
 
-add('pizza', 2);
-add('pizza', 5);
-add('pizza', 4);
+// add('pizza', 2);
+// add('pizza', 5);
+// add('pizza', 4);
 
-console.log(cart);
+// console.log(cart);
+
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+// console.log('Start fetching');
+// const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+// const data = await res.json();
+// console.log(data[0]);
+// console.log('Something');
+
+// import './shoppingCart.js';
+// const getLastPost = async function () {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+//   const data = await res.json();
+//   console.log(data);
+//   return { title: data.at(-1).title, text: data.at(-1).body };
+// };
+
+// const lastPost = getLastPost();
+// console.log(lastPost);
+// // not very clean
+// // lastPost.then(last => console.log(last));
+
+// // user top level await
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,6 +1,11 @@
 // Exporting module
 console.log('Exporting module');
 
+// // blocking code
+// console.log('start fetching user');
+// const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+// console.log('finish fetching');
+
 const shippingCost = 10;
 export const cart = [];
 
